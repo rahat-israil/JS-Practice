577e5597f15af79e8397d5aa7fd7ad2878ede761
rahat-israil <rahat.bin.israil@gmail.com>
2026-02-02 20:13:19 +0600

    let & const

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-let name = 'Rahat';
-console.log(name);
+// let, const
+let myName = "Rahat";
+//console.log(myName);
+myName = "Sara";
+console.log(myName);
+
+
+const country = "Bangladesh";
+const dualName = country + " " + "Short name: BD";
+console.log(dualName);
+
+
+const shopping = ['shirt', 'pant','Jacket']
+shopping.push('panjabi')  // Alow
+console.log(shopping);
+
+
+const student = {
+    name: 'Rahat',
+    class: 'University',
+    Marks: 3.44
+}
+student.Marks = 3.50;   // Allow
+console.log(student);
